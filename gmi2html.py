0caa0b2adc3a6187a880d45fd53de862d5482370
flat <127520563+flatfuzzball@users.noreply.github.com>
2024-06-22 11:41:31 +0200

    Create gmi2html.py

---
+++ gmi2html.py
@@ -0,0 +1,55 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename')
+args = parser.parse_args()
+filename = args.filename
+pre = False
+style = 'style.css'
+with open(filename, 'r') as f:
+    lines = f.readlines()
+with open(filename.rsplit('.', 1)[0] + '.html', 'w') as f:
+    f.write('<!DOCTYPE html>\n<html>\n<head>')
+    if style:
+        f.write(f'<link href="{style}" rel="stylesheet">')
+    f.write('</head>\n<body>')
+    for line in lines:
+        if pre:
+            match line:
+                case '```\n':
+                    f.write('</pre>')
+                    pre = False
+                case other:
+                    f.write(line)
+        else:
+            match line[:2]:
+                case '=>':
+                    ln = line.split(' ', 2)
+                    f.write(f'<a href="{ln[1].strip()}">{ln[2].strip()}</a>')
+                case '# ':
+                    ln = line.split(' ', 1)
+                    f.write(f'<h1>{ln[1]}</h1>')
+                case '##':
+                    ln = line.split(' ', 1)
+                    if line[2] == '#':
+                        h = 3
+                    else:
+                        h = 2
+                    f.write(f'<h{h}>{ln[1]}</h{h}>')
+                case '> ':
+                    ln = line.split(' ', 1)
+                    f.write(f'<blockquote>{ln[1]}</blockquote>')
+                case '``':
+                    if line[2] == '`':
+                        pre = True
+                        f.write('<pre>')
+                    else:
+                        f.write(f'<p>{line}</p>')
+                case '* ':
+                    ln = line.split(' ', 1)
+                    f.write(f'<li>{ln[1]}</li>')
+                case other:
+                    if not line == '\n':
+                        f.write(f'<p>{line}</p>')
+            f.write('\n')
+    f.write('</body>\n</html>')
